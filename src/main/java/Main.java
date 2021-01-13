d990a57b9eccf50a34a9f3b8d47c0687246299e4
AfzalIsmail <47380696+AfzalIsmail@users.noreply.github.com>
2021-01-13 14:12:55 +0400

    Third commit
    - Added comments to the code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,22 +18,36 @@ import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+/**
+ * Main class
+ */
 public class Main extends Application {
 
     private Stage mainWindow;
 
+    /**
+     * Main function
+     * @param args
+     */
     public static void main(String[] args) {
 
         launch(args);
 
     }
 
+    /**
+     * Function to start and show the UI
+     * @param primaryStage
+     * @throws IOException
+     */
     public void start(Stage primaryStage) throws IOException{
 
         mainWindow = primaryStage;
 
+        //Main layout
         BorderPane borderPane = new BorderPane();
 
+        //Tabs for searching/getting and posting JSON
         TabPane tabPane = new TabPane();
 
         Tab search = new Tab("Search");
@@ -60,13 +74,20 @@ public class Main extends Application {
 
     }
 
+    /**
+     * Method to return a VBox that returns the contents for the search tab.
+     * @param stage to open the file dialog to export posts to CSV
+     * @return VBox content for the search tab
+     */
     private VBox searchTabContent(Stage stage){
 
         VBox content = new VBox(10);
         //content.setMaxSize(500,750);
 
+        //---------------------------------------------------------------search by post label
         Label searchLine = new Label("Search 'posts' by: ");
 
+        //---------------------------------------------------------------radio buttons to specify search
         RadioButton id = new RadioButton("ID");
         RadioButton userId = new RadioButton("userID");
         RadioButton title = new RadioButton("title");
@@ -81,18 +102,23 @@ public class Main extends Application {
         HBox selection = new HBox(10);
         selection.getChildren().addAll(id, userId, title);
 
+        //---------------------------------------------------------------text filed to receive user input
         TextField searchPosts = new TextField();
 
+        //---------------------------------------------------------------checkbox to filter query by sorting title in ascending order
         CheckBox sortTitle = new CheckBox("Sort by 'title'");
+        //---------------------------------------------------------------checkbox to filter query by searching for specific words in body
         CheckBox bodyFilter = new CheckBox("Filter body with key words:");
-
+        //---------------------------------------------------------------text field to receive user input to filter body
         TextField bodyFilterField = new TextField();
-
+        //---------------------------------------------------------------button to start query
         Button searchPostsButton = new Button("Search Posts");
 
+        //---------------------------------------------------------------search by comments
         Label searchComments = new Label("\nSearch 'comments' by: \n" +
                                         "Post ID: ");
 
+        //---------------------------------------------------------------radio buttons to specify search query
         RadioButton commentPostId = new RadioButton("Post ID");
         RadioButton commentId = new RadioButton("Comment ID");
         RadioButton commentName = new RadioButton("Comment Name");
@@ -104,15 +130,19 @@ public class Main extends Application {
         commentName.setToggleGroup(searchCommentsBy);
         commentEmail.setToggleGroup(searchCommentsBy);
 
+        //---------------------------------------------------------------text field to specify the postId comments to be queried
         TextField postId = new TextField();
 
         HBox commentSelection = new HBox(10);
         commentSelection.getChildren().addAll(commentPostId, commentId, commentName, commentEmail);
 
+        //---------------------------------------------------------------text field to specify specific words to filter body of JSON objects
         TextField searchCommentsField = new TextField();
 
+        //---------------------------------------------------------------button to start search query
         Button searchCommentsButton = new Button("Search Comments");
 
+        //---------------------------------------------------------------Area to display the results of the query
         Label searchResult = new Label("\nResluts:");
         VBox result = new VBox();
         result.setMaxHeight(180);
@@ -124,8 +154,10 @@ public class Main extends Application {
         resultsPane.setHmax(450);
         result.getChildren().addAll(resultsPane);
 
+        //---------------------------------------------------------------button to export searched posts to CSV
         Button exportData = new Button("Export to CSV");
 
+        //---------------------------------------------------------------Button event for when the searchPost button is pressed
         searchPostsButton.setOnAction(e -> {
 
             String search = searchPosts.getText();
@@ -134,13 +166,17 @@ public class Main extends Application {
 
             URL urlGet = null;
 
+            //---------------------------------------------------------------setting the URL depending on which radio button is selected to query the JSON server
             if(id.isSelected()){
 
                 try {
 
                     urlGet = new URL("https://jsonplaceholder.typicode.com/posts?id="+search);
 
-                }catch (Exception e1){}
+                }catch (Exception e1){
+
+                    System.out.println(e1.getMessage());
+                }
 
             }if(userId.isSelected()){
 
@@ -148,7 +184,7 @@ public class Main extends Application {
 
                     urlGet = new URL("https://jsonplaceholder.typicode.com/posts?userId="+search);
 
-                }catch (Exception e1){}
+                }catch (Exception e1){System.out.println(e1.getMessage());}
 
             }if(title.isSelected()){
 
@@ -156,7 +192,7 @@ public class Main extends Application {
 
                     urlGet = new URL("https://jsonplaceholder.typicode.com/posts?title="+searchTitle);
 
-                }catch (Exception e1){}
+                }catch (Exception e1){System.out.println(e1.getMessage());}
 
             }
 
@@ -164,6 +200,7 @@ public class Main extends Application {
 
                 String line = null;
 
+                //---------------------------------------------------------------setting up connection
                 HttpURLConnection connection = (HttpURLConnection) urlGet.openConnection();
                 connection.setRequestMethod("GET");
 
@@ -184,6 +221,7 @@ public class Main extends Application {
 
                     String responseString = response.toString();
 
+                    //---------------------------------------------------------------Converting the JSON string to a Java object using the GSON Maven repository
                     Gson gson = new Gson();
                     Type postsListType = new TypeToken<ArrayList<Posts>>(){}.getType();
                     ArrayList<Posts> posts = gson.fromJson(responseString, postsListType);
@@ -212,7 +250,7 @@ public class Main extends Application {
 
                                 try {
                                     export(temp, stage);
-                                }catch (IOException e2){}
+                                }catch (IOException e2){System.out.println(e2.getMessage());}
 
                             });
 
@@ -224,7 +262,7 @@ public class Main extends Application {
 
                                 try {
                                     export(temp, stage);
-                                }catch (IOException e2){}
+                                }catch (IOException e2){System.out.println(e2.getMessage());}
 
                             });
 
@@ -252,7 +290,7 @@ public class Main extends Application {
 
                                 try {
                                     export(posts, stage);
-                                }catch (IOException e2){}
+                                }catch (IOException e2){System.out.println(e2.getMessage());}
 
                             });
 
@@ -292,7 +330,7 @@ public class Main extends Application {
 
                     urlGet = new URL("https://jsonplaceholder.typicode.com/posts/" + postIdVal + "/comments?postId=" + search);
 
-                }catch (Exception e1){}
+                }catch (Exception e1){System.out.println(e1.getMessage());}
 
             }if(commentId.isSelected()){
 
@@ -300,7 +338,7 @@ public class Main extends Application {
 
                     urlGet = new URL("https://jsonplaceholder.typicode.com/posts/" + postIdVal + "/comments?id=" + search);
 
-                }catch (Exception e1){}
+                }catch (Exception e1){System.out.println(e1.getMessage());}
 
             }if(commentName.isSelected()){
 
@@ -308,7 +346,7 @@ public class Main extends Application {
 
                     urlGet = new URL("https://jsonplaceholder.typicode.com/posts/" + postIdVal + "/comments?name=" + searchTitle);
 
-                }catch (Exception e1){}
+                }catch (Exception e1){System.out.println(e1.getMessage());}
 
             }if(commentEmail.isSelected()){
 
@@ -316,7 +354,7 @@ public class Main extends Application {
 
                     urlGet = new URL("https://jsonplaceholder.typicode.com/posts/" + postIdVal + "/comments?email=" + search);
 
-                }catch (Exception e1){}
+                }catch (Exception e1){System.out.println(e1.getMessage());}
 
             }
 
@@ -354,6 +392,7 @@ public class Main extends Application {
 
             }catch (Exception e1){
 
+                System.out.println(e1.getMessage());
 
             }
 
@@ -365,12 +404,19 @@ public class Main extends Application {
 
     }
 
+    /**
+     * Method to return the contents of the post tab
+     * @return VBox with content for the post tab
+     * @throws IOException
+     */
     private VBox postTabContent() throws IOException {
 
         VBox content = new VBox();
 
+        //---------------------------------------------------------------specifying JSON server
         URL urlPost = new URL("https://jsonplaceholder.typicode.com/posts/" );
 
+        //---------------------------------------------------------------Input fields for the user to create and send a new JSON object
         Label userId = new Label("User ID");
         TextField userIdField = new TextField();
         //int userIdVal = Integer.parseInt(userIdField.getText());
@@ -387,6 +433,7 @@ public class Main extends Application {
 
         Button post = new Button("Post");
 
+        //---------------------------------------------------------------Area to display the status of the post
         Label searchResult = new Label("Resluts:");
         VBox result = new VBox();
         ScrollPane resultsPane = new ScrollPane();
@@ -397,13 +444,17 @@ public class Main extends Application {
         resultsPane.setHmax(450);
         result.getChildren().addAll(resultsPane);
 
+        //--------------------------------------------------------------- event to start the post query
         post.setOnAction(e ->{
 
+            //---------------------------------------------------------------validating the user input to prevent a post from having a null 'title' field
             if(titleField.getText().length() == 0){
 
                 results.setText("Please enter a 'title' value.");
 
-            }if(bodyField.getText().length() > 400){
+            }
+            //---------------------------------------------------------------validating that the body of the JSON object is less than 400 characters
+            if(bodyField.getText().length() > 400){
 
                 results.setText("The length of the body exceeds 400 characters.");
 
@@ -414,8 +465,8 @@ public class Main extends Application {
                         "   \"title\": \"" + titleField.getText() + "\", \r\n" +
                         "   \"body\": \"" + bodyField.getText() + "\" \n}";
 
-                //System.out.println(postParams);
 
+                //---------------------------------------------------------------Connecting to server
                 try {
 
                     HttpURLConnection postConnection = (HttpURLConnection) urlPost.openConnection();
@@ -449,6 +500,7 @@ public class Main extends Application {
 
                         System.out.println(response.toString());
 
+                        //---------------------------------------------------------------Displaying the results of the post
                         results.setText("Post response code: " + responseCode + "\n\n" +
                                 "Post response message: " + postConnection.getResponseMessage() + "\n\n" +
                                 response.toString());
@@ -461,6 +513,7 @@ public class Main extends Application {
 
                 } catch (Exception e1) {
 
+                    System.out.println(e1.getMessage());
 
                 }
             }
@@ -474,8 +527,15 @@ public class Main extends Application {
 
     }
 
+    /**
+     * Function to export the posts displayed on the UI as CSV
+     * @param posts Arraylists of Posts objects that were searched and that will be written in the CSV file
+     * @param stage the UI stage where the file saving window will be opened
+     * @throws IOException
+     */
     private static void export(ArrayList<Posts> posts, Stage stage) throws IOException{
 
+        //---------------------------------------------------------------Choose the location to save the file
         FileChooser fileChooser = new FileChooser();
 
         File file = fileChooser.showSaveDialog(stage);
@@ -488,6 +548,7 @@ public class Main extends Application {
 
         }
 
+        //---------------------------------------------------------------Writing the headers
         FileWriter fileWriter = new FileWriter(filePath);
 
         fileWriter.append("UserID");
@@ -499,6 +560,7 @@ public class Main extends Application {
         fileWriter.append("body");
         fileWriter.append("\n");
 
+        //---------------------------------------------------------------writing the instances for each Posts object and separating them with ","
         for(Posts p:posts){
 
             fileWriter.append(Integer.toString(p.getUserId()));
--- src/main/java/Posts.java
+++ src/main/java/Posts.java
@@ -5,6 +5,10 @@ public class Posts implements Comparable<Posts>{
     private String title;
     private String body;
 
+    /**
+     * Posts class
+     * The JSON objects are converted to the Java objects based on this class
+     */
     public Posts(){
 
         userId = 0;
@@ -14,38 +18,74 @@ public class Posts implements Comparable<Posts>{
 
     }
 
+    /**
+     * Get the userId
+     * @return userId
+     */
     public int getUserId() {
         return userId;
     }
 
+    /**
+     * Set userId
+     * @param userId
+     */
     public void setUserId(int userId) {
         this.userId = userId;
     }
 
+    /**
+     * Get id
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Set id
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Get title
+     * @return title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Set title
+     * @param title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Get body
+     * @return body
+     */
     public String getBody() {
         return body;
     }
 
+    /**
+     * set body
+     * @param body
+     */
     public void setBody(String body) {
         this.body = body;
     }
 
+    /**
+     * To print the Posts object
+     * @return String of the Posts object
+     */
     @Override
     public String toString(){
 
@@ -56,6 +96,12 @@ public class Posts implements Comparable<Posts>{
 
     }
 
+    /**
+     * Compare the title of one posts object to another
+     * Used for sorting the Posts in ascending order depending on their title
+     * @param p Posts object to compare with
+     * @return The Posts object with the title in ascending order
+     */
     @Override
     public int compareTo(Posts p){
 
